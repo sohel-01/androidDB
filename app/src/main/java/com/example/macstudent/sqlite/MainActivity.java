2d456c7233739fe3ea440a7307476f4b5efe980e
MacStudent <macstudent@20X-iMacXX-M-477.local>
2018-06-25 17:24:32 -0400

    db2

--- app/src/main/java/com/example/macstudent/sqlite/MainActivity.java
+++ app/src/main/java/com/example/macstudent/sqlite/MainActivity.java
@@ -3,28 +3,42 @@ package com.example.macstudent.sqlite;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.macstudent.sqlite.db.DBHelper;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtLable)
+    TextView txtLable;
+    @BindView(R.id.imgText)
+    ImageView imgText;
+    @BindView(R.id.btnSignUp)
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         DBHelper mDBHelper = new DBHelper(this);
         SQLiteDatabase mSQLiteDataBase = mDBHelper.getWritableDatabase();
 
         //Insert Record
         //INSERT INTO tblStudent VALUES(1,"Sohel md")
         ContentValues mContentValues = new ContentValues();
-        mContentValues.put("sid",1);
-        mContentValues.put("snm","Sohel Md");
-        mSQLiteDataBase.insert("tblStudent","null",mContentValues);
+        mContentValues.put("sid", 1);
+        mContentValues.put("snm", "Sohel Md");
+        mSQLiteDataBase.insert("tblStudent", "null", mContentValues);
 
        /* //update
         //UPDATE tblStudent snm = "mdsp" WHERE sid = 1
@@ -40,15 +54,19 @@ public class MainActivity extends AppCompatActivity {
         */
 
         //SELECT Records
-        Cursor mCursor = mSQLiteDataBase.query("tblStudent",null,null,null,null,null,null);
+        Cursor mCursor = mSQLiteDataBase.query("tblStudent", null, null, null, null, null, null);
 
-        if (mCursor != null){
-            if(mCursor.getCount() != 0){
-                while (mCursor.moveToNext()){
-                    Log.d("DB",mCursor.getInt(0)+mCursor.getString((mCursor.getColumnIndex("snm"))));
+        if (mCursor != null) {
+            if (mCursor.getCount() != 0) {
+                while (mCursor.moveToNext()) {
+                    Log.d("DB", mCursor.getInt(0) + mCursor.getString((mCursor.getColumnIndex("snm"))));
                 }
             }
         }
+mSQLiteDataBase.close();
+    }
+    @OnClick(R.id.btnSubmit)
+    public void onViewClicked(){
 
     }
 }
